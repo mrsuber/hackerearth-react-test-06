a492d93bb10fa928f04ad90df44018efc8d1d9de
mohamad <msiysinyuy@gmail.com>
2022-03-06 16:58:19 +0100

    data can be fetch form api

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import {useEffect} from 'react'
+import axios from 'axios'
+
+
 
 const App=()=> {
+  let url = 'https://smartqdemo.firebaseio.com/events-data.json'
+
+
+  useEffect(()=>{
+    const fetchData = async() => {
+      const res = await axios.get(url)
+      console.log(res.data)
+    }
+    fetchData()
+
+
+  },[url])
   return (
 
       <div className='App'>
